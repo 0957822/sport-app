a02d13a61c3843e63ac90394ac5ec1a0d5404b44
Indra Doerga <0957822@hr.nl>
2024-11-19 19:44:09 +0100

    set up routes
    set up hello, oefeningen about and ecersices id

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="bg-white p-6 rounded-lg shadow">
+        <h1 class="text-2xl font-bold mb-4">About Sport-App</h1>
+        <p>Write your about content here...</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\PageController;
 
-// routes/web.php
-
-use App\Http\Controllers\HomeController;
-
-Route::get('/', [App\Http\Controllers\HelloController::class, 'index']);
-
+Route::get('/', [HelloController::class, 'index']);
+Route::get('/oefeningen', [ExerciseController::class, 'index'])->name('exercises');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/exercise/{id}', [ExerciseController::class, 'show'])->name('exercise.show');
