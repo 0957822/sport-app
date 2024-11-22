cfe62a0e2fbe661d72dd2e73f5ca8188bc0149ef
Indra Doerga <0957822@hr.nl>
2024-11-22 20:18:47 +0100

    made pages
    made about & exercises pages

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Controllers\HelloController;
-use App\Http\Controllers\ExerciseController;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ExercisesController;
+use App\Http\Controllers\AboutController;
 
 Route::get('/', [HelloController::class, 'index']);
-Route::get('/oefeningen', [ExerciseController::class, 'index'])->name('exercises');
-Route::get('/about', [PageController::class, 'about'])->name('about');
-Route::get('/exercise/{id}', [ExerciseController::class, 'show'])->name('exercise.show');
+Route::get('/exercises', [ExercisesController::class, 'index'])->name('exercises');
+Route::get('/about', [AboutController::class, 'about'])->name('about');
+Route::get('/exercises/{id}', [ExercisesController::class, 'show'])->name('exercises.show');
