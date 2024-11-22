44fba13e93f20f17aa163a2d749f683a4a57c001
Indra Doerga <0957822@hr.nl>
2024-11-22 20:46:41 +0100

    made pages
    made pages login & register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Register</title>
 </head>
 <body>
-<h1>Welcome to Sport-App</h1>
+<h1>Register</h1>
 </body>
 </html>
-
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ExercisesController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', [HelloController::class, 'index']);
-Route::get('/exercises', [ExercisesController::class, 'index'])->name('exercises');
+Route::get('/exercises', [ExercisesController::class, 'exercises'])->name('exercises');
 Route::get('/about', [AboutController::class, 'about'])->name('about');
 Route::get('/exercises/{id}', [ExercisesController::class, 'show'])->name('exercises.show');
+Route::get('/login/{id}', [LoginController::class, 'login'])->name('login');
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
